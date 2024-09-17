8311d4c653324a4811233b7a4223b3d3fd24593c
jason_tung <yokarilight@gmail.com>
2024-09-17 23:59:39 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org → lll
         </a>
       </footer>
     </div>
